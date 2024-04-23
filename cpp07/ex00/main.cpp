348a4838d5229ab21f5148d1b4e85921c8eab5dd
Jiajia Chen <jiajchen@f1r7s1.codam.nl>
2024-04-23 15:22:52 +0200

    cpp07 ex00 update

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,24 +6,29 @@
 /*   By: jiajchen <jiajchen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/18 12:07:46 by jiajchen      #+#    #+#                 */
-/*   Updated: 2024/04/18 12:08:24 by jiajchen      ########   odam.nl         */
+/*   Updated: 2024/04/22 18:08:34 by jiajchen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 int	main( void ) {
+	
 	int	a = 2;
 	int	b = 3;
+	
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	
 	return (0);
 }
--- cpp07/ex00/whatever.hpp
+++ cpp07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: jiajchen <jiajchen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/04/18 12:06:39 by jiajchen      #+#    #+#                 */
-/*   Updated: 2024/04/18 12:08:41 by jiajchen      ########   odam.nl         */
+/*   Updated: 2024/04/22 18:07:32 by jiajchen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,3 +14,20 @@
 
 #include <iostream>
 #include <string>
+
+template <typename T> T swap(T x, T y)
+{
+	T	temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T> T min(T x, T y)
+{
+	return (x < y) ? x : y;
+}
+
+template <typename T> T max(T x, T y)
+{
+	return (x > y) ? x : y;
+}
