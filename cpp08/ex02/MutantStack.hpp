c75dd68d82954cfa1c220795625a98b3c1f4bf0a
Jiajia Chen <jiajchen@f1r6s9.codam.nl>
2024-10-28 15:35:56 +0100

    cpp09 ex02

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,19 +1,32 @@
 #pragma once
 
+#define RED "\033[91m"
+#define GREEN "\033[92m"
+#define PINK "\033[95m"
+#define RESET "\033[0m"
+
 #include <iostream>
 #include <deque>
 #include <stack>
 
 template <typename T>
-class MutantStack : public std::stack
+class MutantStack : public std::stack<T>
 {
-private:
-	/* data */
 public:
 	typedef typename std::deque<T>::iterator iterator;
+	typedef typename std::deque<T>::const_iterator const_iterator;
+	typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+	typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
 
-	iterator begin();
-	iterator end();
-	iterator
+	iterator begin() { return this->c.begin(); }
+	iterator end() { return this->c.end(); }
+	const_iterator cbegin() const { return this->c.cbegin(); }
+	const_iterator cend() const { return this->c.cend(); }
+	reverse_iterator rbegin() { return this->c.rbegin(); }
+	reverse_iterator rend() { return this->c.rend(); }
+	const_reverse_iterator crbegin() const { return this->c.crbegin(); }
+	const_reverse_iterator crend() const { return this->c.crend(); }
 };
+
+//https://stackoverflow.com/questions/73440733/how-can-i-implement-my-own-stack-iterator-in-c
 
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,26 +1,87 @@
-
+#include "MutantStack.hpp"
+#include <list>
 
 int main(void) {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
+	std::cout << PINK "----Subject Test----" RESET << std::endl;
 	{
-	std::cout << *it << std::endl;
-	++it;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << "Top of mstack: " << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << "Size of mstack: " << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		std::cout << GREEN "[print stack using iterator]: " RESET << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	std::cout << PINK "----List Test----" RESET << std::endl;
+	{
+		std::list<int> mlist;
+		mlist.push_back(5);
+		mlist.push_back(17);
+		std::cout << "Last of mlist: " << mlist.back() << std::endl;
+		mlist.pop_back();
+		std::cout << "Size of mlist: " << mlist.size() << std::endl;
+		mlist.push_back(3);
+		mlist.push_back(5);
+		mlist.push_back(737);
+		//[...]
+		mlist.push_back(0);
+		std::list<int>::iterator it = mlist.begin();
+		std::list<int>::iterator ite = mlist.end();
+		++it;
+		--it;
+		std::cout << GREEN "[print list using iterator]: " RESET << std::endl;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::list<int> l(mlist);
+	}
+	std::cout << PINK "----Reverse iterator Test----" RESET << std::endl;
+	{
+		MutantStack<int> mstack;
+		mstack.push(123);
+		mstack.push(456);
+		MutantStack<int> mstack_dup(mstack);
+		std::cout << "Size of mstack: " << mstack.size() << std::endl;
+		std::cout << "Size of mstack duplicate: " << mstack_dup.size() << std::endl;
+		mstack.push(789);
+		mstack.push(0);
+		MutantStack<int>::const_iterator cit = mstack.cbegin();
+		MutantStack<int>::const_iterator cite = mstack.cend();
+		++cit;
+		--cit;
+		std::cout << GREEN "[print stack using const_iterator]: " RESET << std::endl;
+		while (cit != cite)
+		{
+			std::cout << *cit << std::endl;
+			++cit;
+		}
+		MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+		MutantStack<int>::reverse_iterator rite = mstack.rend();
+		++rit;
+		--rit;
+		std::cout << GREEN "[print stack using reverse_iterator]: " RESET << std::endl;
+		while (rit != rite)
+		{
+			std::cout << *rit << std::endl;
+			++rit;
+		}
+	}
 	return 0;
 }
